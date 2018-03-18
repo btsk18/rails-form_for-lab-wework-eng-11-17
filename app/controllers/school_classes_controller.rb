1fe9a95e516fed5d6797b42d57133fa027f733ba
Brandon Leeds <brandonleeds@gmail.com>
2018-03-18 18:33:04 +0000

    automatically backed up by learn

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,27 @@
+class SchoolClassesController < ApplicationController
+
+  def create
+    @school_class = SchoolClass.new
+    @school_class.title = params[:title]
+    @school_class.room_number = params[:school_number]
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
+
+  def new
+  end
+
+  def show
+    @school_class = SchoolClass.find_by_id(params[:id])
+  end
+
+  def edit
+    @school_class = SchoolClass.find_by_id(params[:id])
+  end
+
+  def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(title: params[:title], room_number: params[:room_number])
+    redirect_to school_class_path(@school_class)
+  end
+end
